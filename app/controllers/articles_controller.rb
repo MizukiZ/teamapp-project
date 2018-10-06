65f74f18d4eec80c00015530fbb1e9b10356117f
peytr <peytr@mail.com>
2018-10-06 15:34:46 +1000

    Complete commments and add authorisation.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "peter", password: "password", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
