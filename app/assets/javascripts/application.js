0b0934fde0355fdffda1f9897861f02c3681de37
peytr <peytr@mail.com>
2018-10-12 22:46:31 +1100

    Add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
 //= require xtreme/articles.index.js
 //= require xtreme/datatables.min.js
 //= require xtreme/jquery.datatables.min.js
