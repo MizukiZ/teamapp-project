c17ef775786f297c566da59a7acf5c4a2a525c5f
peytr <peytr@mail.com>
2018-10-29 15:43:55 +1100

    Refactor signin / registration page to correctly follow criteria

--- app/assets/javascripts/firebase.js
+++ app/assets/javascripts/firebase.js
@@ -3,10 +3,16 @@ var uiConfig = {
   signInSuccessUrl: '/teams',
   signInOptions: [
     // Providers.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID
-  ]
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url
+  tosUrl: 'https://www.teamapp.com/terms',
+  // Privacy policy url
+  privacyPolicyUrl: 'https://www.teamapp.com/privacy'
 };
 
 // Initialize the FirebaseUI Widget using Firebase.
@@ -21,7 +27,7 @@ initApp = function() {
   firebase.auth().onAuthStateChanged(
     function(user) {
       if (user) {
-        // User is signed in. (none of this is being used, this needs to be saved to user db)
+        // User is signed in. (this data needs to be saved to user db)
         var displayName = user.displayName;
         var email = user.email;
         var emailVerified = user.emailVerified;
@@ -31,8 +37,6 @@ initApp = function() {
         var providerData = user.providerData;
         user.getIdToken().then(function(accessToken) {
           document.getElementById('sign-in-status').textContent = 'Signed in as: ' + `${user.displayName}`;
-          //FIXME: added style.display none to stop auth container rendering after login, auth container not showing unless page is refreshed after logout though. This needs to be rectified.
-          document.getElementById('firebaseui-auth-container').style.display = 'none';
           document.getElementById('sign-in').textContent = 'Sign out';
           document.getElementById('account-details').textContent = JSON.stringify({
             displayName: displayName,
@@ -44,32 +48,12 @@ initApp = function() {
             accessToken: accessToken,
             providerData: providerData
           }, null, '  ');
-
         });
-
-
-          //TODO: not sure why this is here, remove if unnecessary
-          document.getElementById('sign-in').addEventListener('click', function() {
-            firebase
-              .auth()
-              .signOut()
-              .then(function() {
-                window.location = '/';
-                alert('You have Signed Out. Please sign In');
-              })
-              .catch(function(error) {
-                // An error happened.
-                alert('An error happened');
-              });
-            // Sign-out successful.
-          });
-
-
       } else {
         // User is signed out.
-        document.getElementById('sign-in-status').style.display = 'none';
-        document.getElementById('sign-in').style.display = 'none';
-        document.getElementById('photo').style.display = 'none';
+        document.getElementById('sign-in-status').style.display = 'Signed out';
+        document.getElementById('sign-in').style.display = 'Sign in';
+        document.getElementById('account-details').style.display = 'null';
       }
     },
     function(error) {
@@ -78,9 +62,31 @@ initApp = function() {
   );
 };
 
+//TODO: sign out process using click event listener taken from demo - delete account function
+document.getElementById('sign-in').addEventListener('click', function() {
+  firebase
+    .auth()
+    .signOut()
+    .then(function() {
+      window.location = '/';
+      alert('You have Signed Out. Please sign In');
+    })
+    .catch(function(error) {
+      // An error happened.
+      alert('An error happened');
+    });
+});
+
+//TODO: confirm with Glen which state
+// signed in until click signed out
+// signed in until window/tab closed
 // User logged in for duration of session
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
 
+//FIXME: added style.display none to stop auth container rendering after login, auth container not showing unless page is refreshed after logout though. This needs to be rectified.
+document.getElementById('firebaseui-auth-container').style.display = 'none';
+
+// Call the FirebaseUI function on document load
 window.addEventListener('turbolinks:load', function() {
   initApp();
 });
